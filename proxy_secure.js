318a31a05482d812cc9ea75c4a7f72b7e53cb8f6
WEASTERNSMITH <98036437+WEASTERNSMITH@users.noreply.github.com>
2026-01-18 16:13:27 +0200

    Update proxy_secure.js

--- proxy_secure.js
+++ proxy_secure.js
@@ -1,9 +1,10 @@
 const Proxy = require("coin-hive-stratum");
 const domain = "yourdomain.com"
 const proxy = new Proxy({
-  host: "pool.supportxmr.com",
-  port: 3333,
+  host: "power2b.na.mine.zpool.ca",
+  port: 6242,
   key: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/privkey.pem"),
   cert: require("fs").readFileSync("/etc/letsencrypt/live/" + domain + "/fullchain.pem"),
 });
+
 proxy.listen(443);
